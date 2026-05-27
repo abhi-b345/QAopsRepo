eaf0bd81ebcddbc91f36a5133d61edcc8a935396
abhi-b345 <tyagi.b345@gmail.com>
2026-05-27 12:58:08 +0530

    file change init

--- clientUiFiles/checkoutPage.js
+++ clientUiFiles/checkoutPage.js
@@ -1,18 +1,27 @@
 class checkoutPage {
     constructor(page, pageUrl, email) {
         this.page = page;
-        this.pageUrl = pageUrl; 
+        this.pageUrl = pageUrl;
         this.email = email;
     }
     async checkout() {
-        await this.page.goto(this.pageUrl);
+        await this.page.goto(this);
         await this.page.getByRole('textbox').nth(1).fill('221');
         await this.page.getByRole('textbox').nth(1).fill('Abhishek');
         await this.page.locator("[name*='coupon']").fill("rahulcoupon");
         await this.page.locator("[class*='input ddl']").first().selectOption("01");
         await this.page.locator("[class*='input ddl']").nth(1).selectOption("11");
         await this.page.getByRole('button', { name: 'Apply Coupon' }).click();
-        await this.page.locator("[placeholder*='Country']").pressSequentially("ind", { delay: 150 });
+        const dropdown = await this.page.locator("[placeholder*='Country']").pressSequentially("ind", { delay: 150 });
+        await dropdown.waitFor();
+        const count = await dropdown.locator("[type*='button']").count();
+        for (let i = 0; i < count; i++) {
+            const text = await dropdown.locator("[type*='button']").nth(i).textContent();
+            if (text === " India") {
+                await text.click();
+                break;
+            }
+        }
         await expect(this.page.getByRole('textbox').nth(4)).toHaveValue(this.email);
         await this.page.getByText('Place Order').click();
 
