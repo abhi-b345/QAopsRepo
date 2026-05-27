ce9995b1669e063f82b4a92d3df50574f2ea9068
abhi-b345 <tyagi.b345@gmail.com>
2026-05-27 14:49:29 +0530

    new file change

--- tests/clientUiRunner.spec.js
+++ tests/clientUiRunner.spec.js
@@ -1,26 +1,29 @@
-const {expect,test} = require('@playwright/test');
-const {loginPage} = require('../clientUiFiles/loginPage');
-const {dashboard} = require('../clientUiFiles/dashboard');
-const {checkoutPage} = require('../clientUiFiles/checkoutPage');
+const { expect, test } = require('@playwright/test');
+const { loginPage } = require('../clientUiFiles/loginPage');
+const { dashboard } = require('../clientUiFiles/dashboard');
+const { checkoutPage } = require('../clientUiFiles/checkoutPage');
+const {orderPlace} = require('../clientUiFiles/orderPlace');
 
-test("test to validate client order", async({page})=>
-{
+test("test to validate client order", async ({ page }) => {
     const email = "tyagi.b345@gmail.com";
     const password = "Abhishek@123";
     const selectItem = "ZARA COAT 3";
     const pageUrl = "https://rahulshettyacademy.com/client/#/auth/login";
-    const loginPageOperation = new loginPage(page,email,password,pageUrl); //calling
+    const loginPageOperation = new loginPage(page, email, password, pageUrl); //calling
     await loginPageOperation.loginOpe(); //calling
     page.waitForLoadState('networkidle');
 
-    const dashboardSelectItem = new dashboard(page,pageUrl,selectItem); ////calling
+    const dashboardSelectItem = new dashboard(page, pageUrl, selectItem); ////calling
     dashboardSelectItem.selectItem(); //calling
-    const cartItem =dashboardSelectItem.cartCheck(); ////calling
+    const cartItem = dashboardSelectItem.cartCheck(); ////calling
     await expect(cartItem).toContain(selectItem);
 
-    const checkoutLogic = new checkoutPage(page);
+    const checkoutLogic = new checkoutPage(page, pageUrl, email);
     await checkoutLogic.checkout();
 
-    
+    const placeOrderLogic = new orderPlace(page, pageUrl, selectItem);
+    await placeOrderLogic.placeOrder();
+
+
 
 })
